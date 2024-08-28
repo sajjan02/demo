b093a1d15e127eb7a48a678bdd23c569a254695c
Prathik Sajjan K S <prathiksajjan.ks@planview.com>
2024-08-28 13:04:24 +0530

    djbdj

---
+++ SampleSolution/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SampleSolution;
+
+Console.WriteLine("Hello, World!");
+
+Calculator calculator = new Calculator();
+
+// Step 7: Call Add and display the result
+int result = calculator.Add(5, 7);
+Console.WriteLine("The result of adding 5 and 7 is: " + result);
